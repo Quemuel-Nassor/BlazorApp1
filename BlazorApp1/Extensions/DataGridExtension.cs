198769cef0379b785b28ca86f4e6d36b74ee5e02
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2023-03-30 08:55:10 -0300

    teste

---
+++ BlazorApp1/Extensions/DataGridExtension.cs
@@ -0,0 +1,47 @@
+using Blazorise.DataGrid;
+using SortDirection = Blazorise.SortDirection;
+
+namespace BlazorApp1.Extensions;
+
+public static class DataGridEventExtension
+{
+    public static string GetGridSorting(this IEnumerable<DataGridColumnInfo> columns)
+    {
+        string direction = string.Empty;
+        DataGridColumnInfo? columnInfo = columns.FirstOrDefault(x => x.SortDirection != SortDirection.Default);
+
+        if (columnInfo == null)
+            return string.Empty;
+
+        switch (columnInfo.SortDirection)
+        {
+            case SortDirection.Ascending:
+                direction = " asc";
+                break;
+            case SortDirection.Descending:
+                direction = " desc";
+                break;
+            default:
+                break;
+        }
+
+        return columnInfo.SortField + direction;
+    }
+
+    public static async Task<Tout> OnReadData<T,Tout,Tin>(this DataGridReadDataEventArgs<T> e, Tin parameters, Func<Tin, Task<Tout>> request) where Tout : new()
+    {
+        if (!e.CancellationToken.IsCancellationRequested)
+        {
+            string sorting = e.Columns.GetGridSorting();
+
+            if (((dynamic)parameters).PageNumber != e.Page || !((dynamic)parameters).Sorting.Equals(sorting))
+            {
+                ((dynamic)parameters).Sorting = sorting;
+                ((dynamic)parameters).PageNumber = e.Page;
+                return await request(parameters);
+            }
+        }
+
+        return new Tout();
+    }
+}
