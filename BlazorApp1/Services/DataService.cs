89486fac45e352e312ac4e36b11e9c7512563283
Quemuel Alves Nassor <quemuel.nassor@novasingular.com.br>
2023-03-31 08:48:25 -0300

    ajustados conteudos

--- BlazorApp1/Services/DataService.cs
+++ BlazorApp1/Services/DataService.cs
@@ -1,49 +1,51 @@
 using System.Linq.Dynamic.Core;
 
-namespace BlazorApp1.Services{
-    public class DataService{
-        private List<Sample> ItemsSrc { get; set; } = new(){
-            new Sample(){
-                Id = 1,
-                Name = "João",
-                CreationTime = DateTime.Now.AddDays(6)
-            },new Sample(){
-                Id = 2,
-                Name = "Maria",
-                CreationTime = DateTime.Now.AddDays(5)
-            },new Sample(){
-                Id = 3,
-                Name = "Antonio",
-                CreationTime = DateTime.Now.AddDays(4)
-            },new Sample(){
-                Id = 4,
-                Name = "Zezinho",
-                CreationTime = DateTime.Now.AddDays(3)
-            },new Sample(){
-                Id = 5,
-                Name = "Julieta",
-                CreationTime = DateTime.Now.AddDays(2)
-            },new Sample(){
-                Id = 6,
-                Name = "Ofelia",
-                CreationTime = DateTime.Now.AddDays(1)
-            },
-        };
+namespace BlazorApp1.Services
+{
+    public class DataService
+    {
+
+        private List<string> Names = new List<string>() { "João", "Maria", "Antonio", "Zezinho", "Julieta", "Ofelia" };
+
+        public DataService()
+        {
+            Source = ItemsSrc();
+        }
+
+        private List<Sample> Source { get; set; }
+
+        private List<Sample> ItemsSrc()
+        {
+            List<Sample> samples = new List<Sample>();
+            Random rnd = new Random(DateTime.Now.Millisecond);
+
+            for (int i = 0; i < 100000; i++)
+            {
+                samples.Add(new Sample
+                {
+                    Id = i,
+                    Name = Names[rnd.Next(0, Names.Count - 1)],
+                    CreationTime = DateTime.Now.AddDays(rnd.Next(1,364))
+                });
+            }
+
+            return samples;
+        }
 
         public async Task<(int total, List<Sample> items)> GetPaged(int page, int pageSize, string sorting)
         {
-            var items = ItemsSrc
+            var items = Source
             .AsQueryable()
             .OrderBy(sorting)
-            .Page(page,pageSize)
+            .Page(page, pageSize)
             .ToList();
 
-            await Task.Delay(300);
+            await Task.Delay(3000);
 
-            return (ItemsSrc.Count, items);
+            return (Source.Count, items);
         }
     }
-    
+
     public class Sample
     {
         public int Id { get; set; }
